a1f1f487615a40473ecff718320bc342ef6cd1fe
Carol <carolinemaee@live.com>
2022-11-14 21:13:02 +1030

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb+srv://cxroline:caroline123@job-tracker.7z3skni.mongodb.net/JobTracker?retryWrites=true&w=majority',
+  process.env.MONGODB_URI || 'mongodb+srv://cxroline:caroline123@JobTracker.7z3skni.mongodb.net/JobTracker?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
