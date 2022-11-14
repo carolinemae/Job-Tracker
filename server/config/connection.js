99b620b3efb03d9973cea19fd5e13170f3ddba09
Carol <carolinemaee@live.com>
2022-11-14 20:45:52 +1030

    url mongodb updated

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/job-tracker',
+  process.env.MONGODB_URI || 'mongodb+srv://cxroline:caroline123@job-tracker.7z3skni.mongodb.net/JobTracker?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
