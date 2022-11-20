2327912cfb984c92b52fcf140e8f60a3d8d5c0f2
Carol <carolinemaee@live.com>
2022-11-21 10:00:29 +1030

    sensitive info removed

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb+srv://cxroline:caroline123@job-tracker.7z3skni.mongodb.net/eichler?retryWrites=true&w=majority',
+  process.env.MONGODB_URI || process.env.DB_CONNECT,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
