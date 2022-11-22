0e8724c90dfc02e150b1bd27361f6ca81c45af95
Carol <carolinemaee@live.com>
2022-11-22 21:35:38 +1030

    db access changed

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 // require('dotenv').config();
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb+srv://cxroline:30tYwtL8CwWuee9H@job-tracker.7z3skni.mongodb.net/eichler?retryWrites=true&w=majority',
+  process.env.MONGODB_URI || 'mongodb+srv://eichler:OOroAsJnQepb0DP@job-tracker.7z3skni.mongodb.net/eichler?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
