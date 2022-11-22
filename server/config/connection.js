2a78b82ea526c6de7e81d2ff39d3259c58975d57
Carol <carolinemaee@live.com>
2022-11-22 21:05:52 +1030

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb+srv://cxroline:cmthomson123@job-tracker.7z3skni.mongodb.net/eichler?retryWrites=true&w=majority',
+  process.env.MONGODB_URI || 'mongodb+srv://cxroline:caroline123@job-tracker.7z3skni.mongodb.net/eichler?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
