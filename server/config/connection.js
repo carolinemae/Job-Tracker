a8b67927550bcade25f688c6931359a7241cedd6
Carol <carolinemaee@live.com>
2022-11-28 14:24:57 +1030

    creds removed from connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
-// require('dotenv').config();
+require('dotenv').config();
 
 mongoose.connect(
-  // process.env.MONGODB_URI || 
-  'mongodb+srv://eichler:OOroAsJnQepb0DP@job-tracker.7z3skni.mongodb.net/eichler?retryWrites=true&w=majority',
+  process.env.MONGODB_URI, 
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
